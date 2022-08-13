18d9cce5bd80d423ee9576feffc9dab80659d07d
neha-butterpaper <neha.joshi@butterpaper.tech>
2022-08-13 14:58:40 +0530

    header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import Badge from "@mui/material/Badge";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Add To Cart</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+
+          <Badge badgeContent={4} color="primary">
+            <i
+              class="fa-solid fa-cart-shopping text-light"
+              style={{ fontSize: 25, cursor: "pointer" }}
+            ></i>
+          </Badge>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
-export default Header
+export default Header;
