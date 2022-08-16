9ed08ef3478e995289155ab0f2054abf2002cf1d
neha-butterpaper <neha.joshi@butterpaper.tech>
2022-08-16 23:42:14 +0530

    style added

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -7,7 +7,7 @@ function CardDetails() {
    <div className='container mt-2'>
     <h2 className='text-center'>Item Details Page</h2>
     <section className="container mt-3">
-        <div className='itemsdetails'>
+        <div className='iteamsdetails'>
             <div className="items_img">
                 <img 
                 src="https://b.zmtcdn.com/data/pictures/3/18514413/0a17b72e9fec52a3ca736f4c2ea3646f_o2_featured_v2.jpg"
@@ -15,6 +15,25 @@ function CardDetails() {
                 />
             </div>
             <div className="details">
+                <Table>
+                    <tr>
+                        <td>
+                            <p>
+                                <strong>Restaurant</strong> : Massala Theory
+                            </p>
+                            <p>
+                                <strong>Restaurant</strong> : Massala Theory
+                            </p>
+                            <p>
+                                <strong>Restaurant</strong> : Massala Theory
+                            </p>
+                            <p>
+                                <strong>Restaurant</strong> : Massala Theory
+                            </p>
+                        </td>
+                    </tr>
+
+                </Table>
             </div>
         </div>
     </section>
